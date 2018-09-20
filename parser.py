078e9600cadfbe87c85481c6fd0b9e5a7bf11e5b
rodrigo zembrzuski <zembrzuski@gmail.com>
2018-09-20 14:23:09 -0300

    ae

---
+++ parser.py
@@ -0,0 +1,17 @@
+import re
+
+def find_pattern(file_content):
+    pattern = re.compile(r'href=\"http://www.rad.cvm.gov.br.+?\"')
+    return pattern.findall(file_content)
+
+def open_file(file_path):
+    file = open(file_path, "r")
+    file_content = file.read()
+    return file_content.replace("\n", " ").replace("\r", " ")
+
+
+if __name__ == '__main__':
+    file_content = open_file("files/example.txt")
+    patterns = find_pattern(file_content)
+
+    print('\n'.join(patterns))
---
+++ readme.md
@@ -0,0 +1,1 @@
+thing big, baby steps
